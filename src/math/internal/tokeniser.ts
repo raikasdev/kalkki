ebfa9e99ae7350a2f55619b70534e7782c8e8612
raikasdev <roni.aikas@gmail.com>
2025-03-02 21:38:50 +0200

    Add support for ** (^)

--- src/math/internal/tokeniser.ts
+++ src/math/internal/tokeniser.ts
@@ -63,10 +63,11 @@ const tokenMatchers = [
 	[
 		// Operators: "-", "+", "/", "*", "^" and "="
 		// The multiplication and minus signs have unicode variants that also need to be handled
-		/^[-=+/*^−×!]/,
+		/^(\*\*|[-=+/*^−×!])/,
 		(str) => ({
 			type: "oper" as const,
 			name: match(str)
+				.with("**", () => "^" as const)
 				.with("-", "+", "/", "*", "^", "!", "=", (op) => op)
 				.with("−", () => "-" as const)
 				.with("×", () => "*" as const)
